ae4d71f722e2ad56f182c89ab7a160ca414fdcb0
Muhammed Thanish <mnmtanish@gmail.com>
2015-11-18 17:14:57 +0530

    Pick all traces

--- src/traces.js
+++ src/traces.js
@@ -1,9 +1,42 @@
 export class TraceStore {
   constructor() {
-
+    this.stores = {};
   }
 
-  isOutlier(/* node */) {
-    return true;
+  addTrace(trace) {
+    const name = trace.name;
+    let store = this.stores[name];
+    if (!store) {
+      store = new Store();
+      this.stores[name] = store;
+    }
+
+    store.addTrace(trace);
+  }
+
+  getOutliers() {
+    const outliers = [];
+    for (const store of this.stores) {
+      outliers.push(...store.getOutliers());
+    }
+
+    return outliers;
+  }
+}
+
+// TODO pick outliers
+export class Store {
+  constructor() {
+    this.traces = [];
+  }
+
+  addTrace(trace) {
+    this.traces.push(trace);
+  }
+
+  getOutliers() {
+    const outliers = this.traces;
+    this.traces = [];
+    return outliers;
   }
 }
